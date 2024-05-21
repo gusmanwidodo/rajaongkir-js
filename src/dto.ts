f144adab12988eac878e2cf34cfad60601cb41a5
Hendra Juli <deulizealand@gmail.com>
2024-05-21 14:25:11 +0800

    Improve > Add Aditional Parameter when get cost

--- src/dto.ts
+++ src/dto.ts
@@ -1,22 +1,20 @@
-import exp from "constants";
-
 export type getProvinceResponseResult = {
   province_id: string;
   province: string;
-}
+};
 
 export type getProvinceResponse = {
   rajaongkir: {
-      query: {
-          id: string;
-      };
-      status: {
-          code: number;
-          description: string;
-      };
-      results: getProvinceResponseResult | getProvinceResponseResult[];
+    query: {
+      id: string;
+    };
+    status: {
+      code: number;
+      description: string;
+    };
+    results: getProvinceResponseResult | getProvinceResponseResult[];
   };
-}
+};
 
 export type getCityResponseResult = {
   city_id: string;
@@ -25,21 +23,21 @@ export type getCityResponseResult = {
   type: string;
   city_name: string;
   postal_code: string;
-}
+};
 
 export type getCityResponse = {
   rajaongkir: {
-      query: {
-          province: string;
-          id: string;
-      };
-      status: {
-          code: number;
-          description: string;
-      };
-      results: getCityResponseResult | getCityResponseResult[];
-  }
-}
+    query: {
+      province: string;
+      id: string;
+    };
+    status: {
+      code: number;
+      description: string;
+    };
+    results: getCityResponseResult | getCityResponseResult[];
+  };
+};
 
 export type getSubDistrictResponseResult = {
   subdistrict_id: string;
@@ -49,20 +47,20 @@ export type getSubDistrictResponseResult = {
   city: string;
   type: string;
   subdistrict_name: string;
-}
+};
 
 export type getSubDistrictResponse = {
   rajaongkir: {
-      query: {
-          city: string;
-      };
-      status: {
-          code: number;
-          description: string;
-      };
-      results: getSubDistrictResponseResult | getSubDistrictResponseResult[];
+    query: {
+      city: string;
+    };
+    status: {
+      code: number;
+      description: string;
+    };
+    results: getSubDistrictResponseResult | getSubDistrictResponseResult[];
   };
-}
+};
 
 export type getCostRequest = {
   origin: string;
@@ -70,47 +68,51 @@ export type getCostRequest = {
   destination: string;
   destinationType: string;
   weight: string;
+  length?: string;
+  width?: string;
+  height?: string;
+  diameter?: string;
   courier: string;
-}
+};
 
 export type getCostResponseResult = {
   code: string;
   name: string;
   costs: Array<{
-      service: string;
-      description: string;
-      cost: Array<{
-          value: number;
-          etd: string;
-          note: string;
-      }>;
+    service: string;
+    description: string;
+    cost: Array<{
+      value: number;
+      etd: string;
+      note: string;
+    }>;
   }>;
-}
+};
 
 export type getCostResponse = {
   rajaongkir: {
-      query: getCostRequest;
-      status: {
-          code: number;
-          description: string;
-      };
-      origin_details: {
-          city_id: string;
-          province_id: string;
-          province: string;
-          type: string;
-          city_name: string;
-          postal_code: string;
-      };
-      destination_details: {
-          subdistrict_id: string;
-          province_id: string;
-          province: string;
-          city_id: string;
-          city: string;
-          type: string;
-          subdistrict_name: string;
-      };
-      results: getCostResponseResult | getCostResponseResult[];
+    query: getCostRequest;
+    status: {
+      code: number;
+      description: string;
+    };
+    origin_details: {
+      city_id: string;
+      province_id: string;
+      province: string;
+      type: string;
+      city_name: string;
+      postal_code: string;
+    };
+    destination_details: {
+      subdistrict_id: string;
+      province_id: string;
+      province: string;
+      city_id: string;
+      city: string;
+      type: string;
+      subdistrict_name: string;
+    };
+    results: getCostResponseResult | getCostResponseResult[];
   };
-}
+};
