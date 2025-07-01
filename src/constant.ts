7813566ea5f9c17aa2dce8e0a535a22548fc27c2
Gusman Widodo <gusmanwidodo@gmail.com>
2025-07-01 12:10:26 +0800

    make base url dynamic

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
-export const starterBaseUrl = 'https://api.rajaongkir.com/starter'
-export const basicBaseUrl = 'https://api.rajaongkir.com/basic'
-export const proBaseUrl = 'https://pro.rajaongkir.com/api'
+export const starterBaseUrl = process.env.RAJAONGKIR_API_STARTER_BASE_URL || 'https://api.rajaongkir.com/starter'
+export const basicBaseUrl = process.env.RAJAONGKIR_API_BASIC_BASE_URL || 'https://api.rajaongkir.com/basic'
+export const proBaseUrl = process.env.RAJAONGKIR_API_PRO_BASE_URL || 'https://pro.rajaongkir.com/api'
 
 export const apiType: string = process.env.RAJAONGKIR_API_TYPE || 'starter'
 export const apiKey: string = process.env.RAJAONGKIR_API_KEY || ''
